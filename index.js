1aa4834f453b48c9ee93f2e8eb899f04218f3dc7
minggas <fasfora@gmail.com>
2019-07-15 23:23:34 -0300

    feat: add the routes for the contact messages

--- index.js
+++ index.js
@@ -22,38 +22,36 @@ app.use(compression());
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  const headers = req.headers['accept-encoding'];  
-  res.set('Cache-Control', 'public, max-age=31557600');
-    res.sendFile(__dirname + "/views/index.html");
-   
+  const headers = req.headers["accept-encoding"];
+  res.set("Cache-Control", "public, max-age=31557600");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/contact", handleSayHello);
-// app.post("/contact", (req, res) => {
-//   res.render("contact", {
-//     name: req.body.name
-//   });
-// });
+app.get("/messages", function(req, res) {
+  Contact.find({}, "name email message -_id", (err, docs) => {
+    if (err) res.status(500).send(err);
+    res.status(200).render("messages", { docs });
+  });
+});
+
+app.post("/contact", function(req, res) {
+  var project = req.params.project;
+  var newContact = new Contact(req.body);
+  newContact
+    .save()
+    .then(result => {
+      res.set("Cache-Control", "public, max-age=31557600");
+      res.status(200).render("contact", {
+        name: req.body.name,
+      });
+    })
+    .catch(err => {
+      res.status(500).render("contact-failure");
+    });
+});
 
 app.use(express.static(__dirname + "/public"));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-function handleSayHello(req, res) {
-  axios.post(`https://formcarry.com/s/9NuF8VICPWm`, {
-    email: req.body.email,
-    name: req.body.name,
-    message: req.body.message
-  })
-  .then(function (response) {
-    res.set('Cache-Control', 'public, max-age=31557600');
-    res.render("contact", {
-      name: req.body.name
-    });
-  })
-  .catch(function (error) {
-    res.render("contact-failure");
-  });
-}
