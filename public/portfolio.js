206852c604b61f9d2ed24678cef91d6c315719fa
minggas <fasfora@gmail.com>
2019-08-26 16:57:44 -0300

    back-up

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,80 +1,80 @@
 (function() {
   "use strict";
   function a() {
+   // c.forEach(e => e.classList.remove('open'));
     this.classList.toggle("open");
     if (this.classList.contains("open")) {
-      if (this.classList.contains("port1")) {
-        this.innerHTML = `<span>A simple Simon Game made as part of FreeCodeCamp course.</span>
-        <h2>Simon Game</h2>
-        <a class="img-port" href="https://minggas.github.io/simon-js/" target="_blank" title="Link to Simon Game"
+      if (this.classList.contains("port3")) {
+        this.innerHTML = `<span>A REST API to store books.</span>
+        <h2>Personal Library</h2>
+        <a class="img-port" href="https://minggas.github.io/fcc-personal-library/" target="_blank" title="Link to Personal Library Project"
           rel="noreferrer">
-          <img src="assets/simon.jpg" alt="Screen of Simon Game app" />
+          <img src="assets/library.jpg" alt="Screen Personal Library" />
         </a>
-        <a href="https://minggas.github.io/simon-js/" class="btn-light" rel="noreferrer">
+        <a href="https://node-library-249212.appspot.com/" class="btn-light" target="_blank" rel="noreferrer">
           <i class="fas fa-eye"></i> Project
         </a>
-        <a href="https://github.com/minggas/simon-js" class="btn-dark" rel="noreferrer">
+        <a href="https://github.com/minggas/fcc-personal-library" target="_blank" class="btn-dark" rel="noreferrer">
           <i class="fab fa-github"></i> Github
         </a>
-        <span>Made with: HTML5, CSS3 and Vanilla JS</span>`;
+        <span>Made with: NodeJs/Express & MongoDb/Mongoose</span>`;
       }
-      if (this.classList.contains("port2")) {
-        this.innerHTML = `<span>A search app for wikipedia made as part of FreeCodeCamp
-        course.</span>
-      <h2>WikiSearch</h2>
-      <a class="img-port" href="https://minggas.github.io/wikisearch/" target="_blank" title="Link to WikiSearch" rel="noreferrer">
-        <img src="assets/wikisearch.jpg" alt="Screen of WikiSearch app" />
+      if (this.classList.contains("port4")) {
+        this.innerHTML = `<span>A weather forecast for your location.</span>
+      <h2>Weather Forecast</h2>
+      <a class="img-port" href="https://minggas.github.io/WeatherForecast" target="_blank" title="Link to Weather Forecast" rel="noreferrer">
+        <img src="assets/weather.jpg" alt="Weather Forecast" />
       </a>
-      <a href="https://minggas.github.io/wikisearch/" class="btn-light" rel="noreferrer">
+      <a href="https://minggas.github.io/WeatherForecast" target="_blank" class="btn-light">
         <i class="fas fa-eye"></i> Project
       </a>
-      <a href="https://github.com/minggas/wikisearch" class="btn-dark" rel="noreferrer">
+      <a href="https://github.com/minggas/WeatherForecast" target="_blank" class="btn-dark" rel="noreferrer">
         <i class="fab fa-github"></i> Github
       </a>
-      <span>Made with: HTML5, CSS3 and JQuery</span>`;
+      <span>Made with: HTML5, CSS3 and Javascript</span>`;
       }
-      if (this.classList.contains("port3")) {
+      if (this.classList.contains("port1")) {
         this.innerHTML = `<span>A pomodoro timer made with React.</span>
         <h2>Pomodoro Timer</h2>
         <a class="img-port" href="https://minggas.github.io/react-pomodoro/" target="_blank" title="Link to Pomodoro Timer"
           rel="noreferrer">
           <img src="assets/pomodoro.jpg" alt="Screen of Pomodoro Timer app" />
         </a>
-        <a href="https://minggas.github.io/react-pomodoro/" class="btn-light" rel="noreferrer">
+        <a href="https://minggas.github.io/react-pomodoro/" class="btn-light" target="_blank" rel="noreferrer">
           <i class="fas fa-eye"></i> Project
         </a>
-        <a href="https://github.com/minggas/react-pomodoro" class="btn-dark" rel="noreferrer">
+        <a href="https://github.com/minggas/react-pomodoro" class="btn-dark" rel="noreferrer" target="_blank" >
           <i class="fab fa-github"></i> Github
         </a>
         <span>Made with: HTML5, CSS3 and React</span>`;
       }
-      if (this.classList.contains("port4")) {
+      if (this.classList.contains("port2")) {
         this.innerHTML = `<span>A React app to generate random quotes</span>
         <h2>Quote-O-Matic</h2>
         <a class="img-port" href="https://minggas.github.io/react-quotes/" target="_blank" title="Link to Quote-O-Matic"
           rel="noreferrer">
           <img src="assets/quotes.jpg" alt="Screen of Quote-O-Matic app" />
         </a>
-        <a href="https://minggas.github.io/react-quotes/" class="btn-light" rel="noreferrer">
+        <a href="https://minggas.github.io/react-quotes/" class="btn-light" rel="noreferrer" target="_blank">
           <i class="fas fa-eye"></i> Project
         </a>
-        <a href="https://github.com/minggas/react-quotes" class="btn-dark" rel="noreferrer">
+        <a href="https://github.com/minggas/react-quotes" class="btn-dark" rel="noreferrer" target="_blank">
           <i class="fab fa-github"></i> Github
         </a>
         <span>Made with: HTML5, SCSS and React</span>`;
       }
     } else {
       if (this.classList.contains("port1")) {
-        this.innerHTML = `<h2>Simon Game</h2>`;
+        this.innerHTML = `<h2>Pomodoro</h2>`;	     
       }
       if (this.classList.contains("port2")) {
-        this.innerHTML = `<h2>WikiSearch</h2>`;
+        this.innerHTML = `<h2>Quote-O-Matic</h2>`;
       }
       if (this.classList.contains("port3")) {
-        this.innerHTML = `<h2>Pomodoro Timer</h2>`;
+        this.innerHTML = `<h2>Personal Library</h2>`;
       }
       if (this.classList.contains("port4")) {
-        this.innerHTML = `<h2>Quote-O-Matic</h2>`;
+        this.innerHTML = `<h2>Weather Forecast</h2>`;
       }
     }
   }
